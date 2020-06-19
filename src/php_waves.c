8fbe618638389c74ccee1fc803ee052abee27319
Patryk Prusek <prusek.patryk@gmail.com>
2020-06-19 14:43:18 +0200

    c

--- src/php_waves.c
+++ src/php_waves.c
@@ -825,11 +825,7 @@ int wallet_ethereum_assemble_tx(EthereumSignTx *msg, EthereumSig *tx, uint64_t *
                           msg->data_initial_chunk.size, new_msg.data_initial_chunk.bytes,
                           &(new_msg.data_initial_chunk.size), false);
 
-    wallet_encode_int(tx->signature_v, &(new_tx.signature_v));
-    wallet_encode_element(tx->signature_r.bytes, tx->signature_r.size,
-                          new_tx.signature_r.bytes, &(new_tx.signature_r.size), true);
-    wallet_encode_element(tx->signature_s.bytes, tx->signature_s.size,
-                          new_tx.signature_s.bytes, &(new_tx.signature_s.size), true);
+
 
     int length = wallet_encode_list(&new_msg, &new_tx, rawTx);
     return length;
