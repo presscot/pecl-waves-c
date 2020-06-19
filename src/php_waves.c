b273f006f601137cb8ab1fcb22acd9a64ce9ea11
Patryk Prusek <prusek.patryk@gmail.com>
2020-06-19 15:35:26 +0200

    c

--- src/php_waves.c
+++ src/php_waves.c
@@ -813,14 +813,14 @@ int wallet_ethereum_assemble_tx(EthereumSignTx *msg, EthereumSig *tx, uint64_t *
 
     wallet_encode_element(msg->nonce.bytes, msg->nonce.size,
                           new_msg.nonce.bytes, &(new_msg.nonce.size), false);
-    wallet_encode_element(msg->gas_price.bytes, msg->gas_price.size,
-                          new_msg.gas_price.bytes, &(new_msg.gas_price.size), false);
-    wallet_encode_element(msg->gas_limit.bytes, msg->gas_limit.size,
-                          new_msg.gas_limit.bytes, &(new_msg.gas_limit.size), false);
-    wallet_encode_element(msg->to.bytes, msg->to.size, new_msg.to.bytes,
-                          &(new_msg.to.size), false);
-    wallet_encode_element(msg->value.bytes, msg->value.size,
-                          new_msg.value.bytes, &(new_msg.value.size), false);
+   // wallet_encode_element(msg->gas_price.bytes, msg->gas_price.size,
+   //                       new_msg.gas_price.bytes, &(new_msg.gas_price.size), false);
+   // wallet_encode_element(msg->gas_limit.bytes, msg->gas_limit.size,
+   //                       new_msg.gas_limit.bytes, &(new_msg.gas_limit.size), false);
+   // wallet_encode_element(msg->to.bytes, msg->to.size, new_msg.to.bytes,
+   //                       &(new_msg.to.size), false);
+    //wallet_encode_element(msg->value.bytes, msg->value.size,
+  //                        new_msg.value.bytes, &(new_msg.value.size), false);
     //wallet_encode_element(msg->data_initial_chunk.bytes,
      //                     msg->data_initial_chunk.size, new_msg.data_initial_chunk.bytes,
      //                     &(new_msg.data_initial_chunk.size), false);
@@ -864,29 +864,29 @@ size_t data_len;
     tx.nonce.size = size_of_bytes(strlen(nonce));
     hex2byte_arr(nonce, strlen(nonce), tx.nonce.bytes, tx.nonce.size);
 
-    tx.gas_price.size = size_of_bytes(strlen(gas_price));
-    hex2byte_arr(gas_price, strlen(gas_price), tx.gas_price.bytes, tx.gas_price.size);
+  //  tx.gas_price.size = size_of_bytes(strlen(gas_price));
+  //  hex2byte_arr(gas_price, strlen(gas_price), tx.gas_price.bytes, tx.gas_price.size);
 
-    tx.gas_limit.size = size_of_bytes(strlen(gas_limit));
-    hex2byte_arr(gas_limit, strlen(gas_limit), tx.gas_limit.bytes, tx.gas_limit.size);
+//    tx.gas_limit.size = size_of_bytes(strlen(gas_limit));
+   // hex2byte_arr(gas_limit, strlen(gas_limit), tx.gas_limit.bytes, tx.gas_limit.size);
 
-    tx.to.size = size_of_bytes(strlen(to));
-    hex2byte_arr(to, strlen(to), tx.to.bytes, tx.to.size);
+   // tx.to.size = size_of_bytes(strlen(to));
+ //   hex2byte_arr(to, strlen(to), tx.to.bytes, tx.to.size);
 
-    tx.value.size = size_of_bytes(strlen(value));
-    hex2byte_arr(value, strlen(value), tx.value.bytes, tx.value.size);
+  //  tx.value.size = size_of_bytes(strlen(value));
+//    hex2byte_arr(value, strlen(value), tx.value.bytes, tx.value.size);
 
-    tx.data_initial_chunk.size = size_of_bytes(strlen(data));
-    hex2byte_arr(data, strlen(data), tx.data_initial_chunk.bytes,
-                 tx.data_initial_chunk.size);
+  //  tx.data_initial_chunk.size = size_of_bytes(strlen(data));
+  //  hex2byte_arr(data, strlen(data), tx.data_initial_chunk.bytes,
+   //              tx.data_initial_chunk.size);
 
-    signature.signature_v = 27;
+    //signature.signature_v = 27;
 
-    signature.signature_r.size = size_of_bytes(strlen(r));
-    hex2byte_arr(r, strlen(r), signature.signature_r.bytes, signature.signature_r.size);
+  //  signature.signature_r.size = size_of_bytes(strlen(r));
+//    hex2byte_arr(r, strlen(r), signature.signature_r.bytes, signature.signature_r.size);
 
-    signature.signature_s.size = size_of_bytes(strlen(s));
-    hex2byte_arr(s, strlen(s), signature.signature_s.bytes, signature.signature_s.size);
+    //signature.signature_s.size = size_of_bytes(strlen(s));
+   // hex2byte_arr(s, strlen(s), signature.signature_s.bytes, signature.signature_s.size);
 
     int length = wallet_ethereum_assemble_tx(&tx, &signature, raw_tx_bytes);
     int8_to_char((uint8_t *) raw_tx_bytes, length, rawTx);
