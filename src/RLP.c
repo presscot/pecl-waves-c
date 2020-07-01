8d9653fb0d35b5d8301364cac4ef65ae67306d86
Patryk Prusek <prusek.patryk@gmail.com>
2020-07-01 13:14:22 +0200

    c

--- src/RLP.c
+++ src/RLP.c
@@ -53,6 +53,7 @@ int rlp_encode_list(uint8_t *rawTx, uint32_t totalLength, list_node_t* list) {
     while( NULL != list ){
         (list->element).pointer;
         (list->element).size;
+        php_printf("sizeof", sizeof((list->element).pointer) );
         php_printf("size : %d\n", (list->element).size );
         php_printf("size : %d\n\n", copyPos );
         copyPos = rlp_copy(data + copyPos, (list->element).pointer, (list->element).size, copyPos);
